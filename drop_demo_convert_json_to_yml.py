a9d6d4338441e90acb611a984f9c4c491d3a9304
ToddLynn <810304762@qq.com>
2021-09-03 09:59:33 +0800

    提交记录-nlu数据集的转换方式验证成功后的备份 提交时间-2021-9-3 09:55

---
+++ drop_demo_convert_json_to_yml.py
@@ -0,0 +1,12 @@
+
+# json->md"成功转换"
+"""
+rasa data convert nlu --data data/M3-training_dataset_1564317234.json --out data/nlu.md -f md
+
+"""
+
+
+# md->yml
+"""
+rasa data convert nlu --data data --out data -f yaml
+"""
